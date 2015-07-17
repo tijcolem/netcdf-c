09c86a405326ff85111c4ca3e0e8dc9ce39ff0f4
dmh <dmh@unidata.ucar.edu>
2015-07-16 21:15:55 -0600

    initial merge of the cdf5 code

--- nc_test/t_nc_p5.c
+++ nc_test/t_nc_p5.c
@@ -357,19 +357,11 @@ main(int argc, char *argv[])
 	size_t chunksz = 8192;
 	size_t align = 8192/32;
 
-#ifdef TEST_PNETCDF
 	MPI_Init(&argc, &argv);
 
         // cmode |= NC_PNETCDF |NC_64BIT_OFFSET;
         cmode != NC_PNETCDF |NC_64BIT_DATA;
 	ret = nc_create_par(fname,cmode, MPI_COMM_WORLD, MPI_INFO_NULL, &id);
-#else
-	ret = nc__create(fname,cmode, initialsz, &chunksz, &id);
-	if(ret != NC_NOERR) {
- 		/* (void) fprintf(stderr, "trying again\n"); */
-		ret = nc__create(fname,cmode, initialsz, &chunksz, &id);
-#endif
-	}
 	if(ret != NC_NOERR)  {
 		fprintf(stderr,"Error %s in file %s at line %d\n",nc_strerror(ret),__FILE__,__LINE__);
 		exit(ret);
@@ -472,12 +464,7 @@ main(int argc, char *argv[])
  *	read it
  */
         omode = NC_NOWRITE;
-#ifdef TEST_PNETCDF
         omode = NC_NOWRITE | NC_PNETCDF;
-#else
-	ret = nc_open_par(fname,omode, MPI_COMM_WORLD, MPI_INFO_NULL, &id);
-	ret = nc__open(fname,omode, &chunksz, &id);
-#endif
 	if(ret != NC_NOERR)
 	{
    	    (void) printf("Could not open %s: %s\n", fname,
@@ -670,8 +657,6 @@ main(int argc, char *argv[])
 	ret = nc_close(id);
 	/* (void) printf("re nc_close ret = %d\n", ret); */
 
-#ifdef TEST_PNETCDF
 	MPI_Finalize();
-#endif
 	return 0;
 }
