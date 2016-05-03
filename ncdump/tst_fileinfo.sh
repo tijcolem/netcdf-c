02beebb9854f76ce4651b0f918318fadefb9dde0
Dennis Heimbigner <dmh@ucar.edu>
2016-05-03 14:50:29 -0600

    ckp

--- ncdump/tst_fileinfo.sh
+++ ncdump/tst_fileinfo.sh
@@ -43,10 +43,10 @@ if ./tst_fileinfo > /dev/null ; then
    N_IS=`echo $N_IS | cut -d= -f2`
    H_IS=`${builddir}/ncdump -s $HDF | fgrep '_IsNetcdf4' | tr -d ' ;'`
    H_IS=`echo $H_IS | cut -d= -f2`
-   if test "x$N_IS" == 'x0' ;then
+   if test "x$N_IS" = 'x0' ;then
      echo "FAIL: $NCF is marked as not netcdf-4"
    fi
-   if test "x$H_IS" == 'x1' ;then
+   if test "x$H_IS" = 'x1' ;then
      echo "FAIL: $HDF is marked as netcdf-4"
    fi
 else
